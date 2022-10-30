8326798b67612b276bffd7539335d581f44b9641
0n3s1d3 <qwertyulor@mail.ru>
2022-10-30 21:56:25 +0500

    created files

---
+++ header.php
@@ -0,0 +1,13 @@
+<header class="header">
+    <div class="container">
+        <div class="header-nav">
+            Шапка
+        </div>
+        <!--    <a href="/" class="header__logo"><img src="" alt=""></a>-->
+        <!--    <form class="search-form">-->
+        <!--        <input type="text" placeholder="Искать здесь...">-->
+        <!--        <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>-->
+        <!--    </form>-->
+        <!--  кнопка входа и корзина  -->
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="partial/css/normalize.css">
+    <link rel="stylesheet" href="partial/css/bootstrap.min.css">
+    <link rel="stylesheet" href="partial/css/all.min.css">
+    <link rel="stylesheet" href="partial/css/style.css">
+    <title>Online Books - Книжный магазин</title>
+</head>
+<body>
+<?php include 'header.php'?>
+
+<?php include 'footer.php'?>
+<script src="partial/js/bootstrap.bundle.min.js"></script>
+<script src="partial/js/all.min.js"></script>
+</body>
+</html>
